c5475163277f4e5c525b6492d792578bb0a2bcd8
kvngstar <kvngsley019@gmail.com>
2023-01-28 22:00:43 +0100

    adjusting chatbox

--- src/route/adminPages/homePage.jsx
+++ src/route/adminPages/homePage.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import $ from "jquery";
 import { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
-import {toast} from "react-toastify"
+import { toast } from "react-toastify";
 import Footer from "../../component/footer";
 import Nav__ from "../../component/nav";
 import TextBox from "../../chatBox/textBox";
 import Image from "../../sources/assets/facebook.png";
 import textdata from "../../chatBox/jsontext";
 import auth from "../../services/authService";
-import  config from "../../config.json"
+import config from "../../config.json";
 import axios from "axios";
 export default function Hompage() {
   const [showIcon, setShowIcon] = useState(true);
-  const [text,setText] = useState("")
+  const [text, setText] = useState("");
   useEffect(() => {
     $(function () {
       $("div.hidd").on("click", function () {
@@ -32,43 +32,38 @@ export default function Hompage() {
     $("div  .chatBox").fadeToggle("3");
     setShowIcon(true);
   }
-  function getInput(event){
-    const {name,value} = event.target;
-    setText(value) 
+  function getInput(event) {
+    const { name, value } = event.target;
+    setText(value);
   }
-  async function submitText(event){
+  async function submitText(event) {
     event.preventDefault();
 
     if (!text.trim() || text.trim() == "") {
       return;
     } else {
-      try{
-        const response = await auth.post( config.apiUrl +
-        "/customer/chats",{text},
-        {
-          "Content-type": "application/json; charset=UTF-8",
+      try {
+        const response = await auth.post(
+          config.apiUrl + "/customer/chats",
+          { text },
+          {
+            "Content-type": "application/json; charset=UTF-8",
+          }
+        );
+        if (response.status === 200) {
+          setText("");
         }
-      );
-      if(response.status === 200){
-
-         setText("")
-      }}
-
-      catch (error) {
+      } catch (error) {
         if (
           error.response &&
           error.response.status >= 400 &&
           error.response.status < 500
         ) {
-      
           return toast.error(error.response.data);
         }
         return toast.error(error.message);
       }
-      
-
     }
-
   }
 
   return (
@@ -96,11 +91,14 @@ export default function Hompage() {
                 return (
                   <div className="textContainer">
                     {v.status === "customer" ? (
-                      <div className="customer_textbox"> 
-                        <div className="d-flex mb-3">
-                        <img src={Image} className="mr-2" style={{ height: "20px" }} />
-                        <div> {v.message}</div>
-                          
+                      <div className="customer_textbox">
+                        <div className="d-flex mb-4">
+                          <img
+                            src={Image}
+                            className="mr-2"
+                            style={{ height: "20px" }}
+                          />
+                          <div> {v.message}</div>
                         </div>
 
                         <span className="chat_date">
@@ -110,11 +108,10 @@ export default function Hompage() {
                       </div>
                     ) : (
                       <div className="admin_textbox ">
-                        <div className="d-flex mb-3">
-                        <img src={Image} className="mr-2" style={{ height: "20px" }} />
-                        <div> {v.message}</div>
-                          
-                        </div> 
+                        <div className="d-flex mb-4">
+                          <span class="material-symbols-outlined mr-2">person</span>
+                          <div> {v.message}</div>
+                        </div>
                         <span className="chat_date">
                           {" "}
                           <span className="admin_label mr-1">admin</span>
@@ -137,7 +134,9 @@ export default function Hompage() {
               />
 
               <div className="input-group-append">
-                <button className="btn btn-info rounded" onClick={submitText}>send</button>
+                <button className="btn btn-info rounded" onClick={submitText}>
+                  send
+                </button>
               </div>
             </div>
           </div>
