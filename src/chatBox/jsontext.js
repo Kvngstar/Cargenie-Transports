a1d61b6a5951c6440902c2a8d6ab5f5371323f92
kvngstar <kvngsley019@gmail.com>
2023-02-13 07:24:35 +0100

    updated db pathway

--- src/chatBox/jsontext.js
+++ src/chatBox/jsontext.js
@@ -1,6 +1,6 @@
-module.exports = [ {status:"admin",message: "How can i help you?", Time: getDate()},
+module.exports = [ {status:"admin",message: "How can i help you?", Time: "now"},
 ]
 function getDate(){
     let todays_date = new Date().toLocaleString()
     return todays_date
-} 
+}  
